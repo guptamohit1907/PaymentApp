b770ec88b35e28e300b0c79b80e55215a8a8cecd
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-06-28 01:36:27 +0530

    Initial Project Setup

---
+++ Payment/Payment/PaymentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentApp.swift
+//  Payment
+//
+//  Created by Mohit Gupta on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PaymentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
